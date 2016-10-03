a39829c8f48bba478adcd63d2bb40d8a7a3f0489
Artem Molonosov <artmol91@gmail.com>
2016-10-03 17:21:17 +0200

    Basic execution

--- cli.js
+++ cli.js
@@ -1,2 +1,35 @@
 'use strict';
 
+// Libs
+var fs = require('fs');
+var assert = require('assert');
+
+// Internal
+var utils = require('./lib/utils');
+var run = require('./lib/run');
+
+// node cli.js problem
+var problem = process.argv.slice(2)[0];
+
+if(!problem) {
+  console.error('\t- You must specify a problem');
+  return process.exit(1);
+}
+
+var runFile = './problems/' + problem + '/' + problem + '.js';
+var testFile = './problems/' + problem + '/' + problem + '.test';
+
+fs.readFile(testFile, function(err, data) {
+  if(err) {
+    throw err;
+  }
+
+  var tests = utils.parseTestSuite(data.toString());
+
+  tests.forEach(function(test) {
+    run(runFile, test.input, function(result) {
+      assert.equal(test.output, result);
+      console.log('OK!');
+    });
+  });
+});
